0b1a1d8ebc165145e0a3baec7c13d691da91136a
tylrk <tylerk1117@gmail.com>
2022-06-02 17:26:50 -0500

    updates

--- pages/components/nftCard.jsx
+++ pages/components/nftCard.jsx
@@ -1,16 +1,31 @@
 export const NFTCard = ({nft}) => {
     return (
-        <div>
-            <div>
-                <img src={nft.media[0].gateway}></img>
+        <div className="flex flex-col w-1/4">
+            <div className="rounded-md">
+                <img className="object-cover h-128 w-full rounded-t-md" src={nft.media[0].gateway}></img>
             </div>
-            <div>
-                <h2>{nft.title}</h2>
-                <p>{nft.id.tokenId.substr(nft.id.tokenId.length - 4)}</p>
-                <p>{`${nft.contract.address.substr(0, 5)}...${nft.contract.address.substr(nft.contract.address.length - 4)}`}</p>
+
+            <div className="flex flex-col y-gap-2 px-2 py-3 bg-slate-100 rounded-b-md h-110">
+                <div className="">
+                    <h2 className="text-xl text-gray-800">{nft.title}</h2>
+                    <p className="text-gray-600">ID: {nft.id.tokenId.substr(nft.id.tokenId.length - 4)}</p>
+                    <p className="inline text-gray-600">{`${nft.contract.address.substr(0, 5)}...${nft.contract.address.substr(nft.contract.address.length - 4)}`}</p>
+                    <button className="inline hover:scale-110" onClick={() =>  navigator.clipboard.writeText(nft.contract.address)}>
+                        <img className=" ml-1 h-4 w-4 " src= "./copy-icon.png" />
+                    </button>
             </div>
-            <div>
-                <p>{nft.description?.substr(0, 150)}</p>
+
+            <div className="flex-grow mt-2">
+                <p className="text-gray-600">{nft.description?.substr(0, 150)}...</p>
+            </div>
+
+            <div className="flex justify-center mb-1 mt-2">
+                <a className="py-2 px-4 bg-blue-500 w-1/2 text-center rounded-md text-white cursor-pointer" 
+                    target={"blank"} 
+                    href={`https://etherscan.io/token/${nft.contract.address}`}
+                >
+                    View on etherscan</a>
+            </div>
             </div>
         </div>
     )
--- pages/index.jsx
+++ pages/index.jsx
@@ -55,26 +55,47 @@ const Home = () => {
   }
 
   return (
-    <div className="flex min-h-screen flex-col items-center justify-center py-2">
-      <div>
-        <input onChange={(e) => {setWalletAddress(e.target.value)}} value={wallet} type={"text"} placeholder="Add your wallet address"></input>
-        <input onChange={(e) => {setCollectionAddress(e.target.value)}} value={collection} type={"text"} placeholder="Add the collection address"></input>
-        <label><input onChange={(e) => {setFetchForCollection(e.target.checked)}} type={"checkbox"}></input>Fetch for collection</label>
-        <button onClick={
-          () => {
-            if(fetchForCollection) {
-              fetchNFTsforCollection()
-            } else {
-              fetchNFTs()
+    <div className="flex flex-col items-center justify-center py-8 gap-y-3">
+      <div className="flex flex-col w-full justify-center items-center gap-y-2">
+        <input className="w-2/5 bg-slate-100 py-2 px-2 rounded-lg text-gray-800 focus:outline-blue-300 disabled:bg-slate-50 disabled:text-gray-50"
+          disabled={fetchForCollection}
+          onChange={(e) => {setWalletAddress(e.target.value)}} 
+          value={wallet} 
+          type={"text"} 
+          placeholder="Add your wallet address"
+        >
+        </input>
+        <input className="w-2/5 bg-slate-100 py-2 px-2 rounded-lg text-gray-800 focus:outline-blue-300 disabled:bg-slate-50 disabled:text-gray-50"
+          onChange={(e) => {setCollectionAddress(e.target.value)}} 
+          value={collection} 
+          type={"text"} 
+          placeholder="Add the collection address"
+        >
+        </input>
+        <label className="text-gray-600">
+          <input className="mr-2"
+            onChange={(e) => {setFetchForCollection(e.target.checked)}} 
+            type={"checkbox"}
+          >
+          </input>Fetch for collection</label>
+        <button className="disabled:bg-slate-500 text-white bg-blue-400 px-4 py-2 mt-3 rounded-md w-1/5"
+          onClick={
+            () => {
+              if(fetchForCollection) {
+                fetchNFTsforCollection()
+              } else {
+                fetchNFTs()
+              }
             }
-          }
-        } >Let's go!</button>
+          } 
+        >
+          Let's go!</button>
       </div>
-      <div>
+      <div className="flex flex-wrap gap-y-12 mt-4 w-5/6 gap-x-2 justify-center">
         {
-          NFTs.length && NFTs.map(nft => {
+          NFTs.length && NFTs.map((nft, index) => {
             return (
-              <NFTCard nft={nft}></NFTCard>
+              <NFTCard nft={nft} key={index}></NFTCard>
             )
           })
         }
