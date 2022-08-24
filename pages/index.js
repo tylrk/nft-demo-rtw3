b46f33c071c5b3250751b5b9300a0ea93d2f21d8
tylrk <tylerk1117@gmail.com>
2022-08-23 19:01:25 -0500

    updates to functionality

--- pages/index.js
+++ pages/index.js
@@ -6,8 +6,7 @@ const Home = () => {
   const [wallet, setWallet] = useState("")
   const [collection, setCollectionAddress] = useState("")
   const [NFTs, setNFTs] = useState([])
-  const [fetchForCollection, setFetchForCollection] = useState(false)
-  const [startToken, setStartToken] = useState('')
+  const [startToken, setStartToken] = useState(null)
   const [showButton, setShowButton] = useState(false)
 
   useEffect(() => {
@@ -29,67 +28,66 @@ const Home = () => {
 
   // Fetch API 
 
-  const fetchNFTs = async () => {
-    let nfts;
-    if(!collection.length) {
-      var requestOptions = {
-        method: 'GET',
-        redirect: 'follow'
-      };
-
-      console.log("fetching nfts");
-   
-      const baseURL = `https://eth-mainnet.alchemyapi.io/v2/${process.env.NEXT_PUBLIC_API_KEY}/getNFTs/`;
-      const fetchURL = `${baseURL}?owner=${wallet}`;
-      nfts = await fetch(fetchURL, requestOptions).then(data => data.json())
-    } /*else {
-        console.log("fetching nfts for collection owned by address")
-        const fetchURL = `${baseURL}?owner=${wallet}&contractAddresses%5B%5D=${collection}`;
-        nfts = await fetch(fetchURL, requestOptions).then(data => data.json())
-    } */
-
-    if(nfts) {
-      console.log("nfts:", nfts);
-      setNFTs([...NFTs, ...nfts.ownedNfts]);
-
-      if(nfts.pageKey) {
-        setStartToken(nfts.pageKey)
-      }
+  const fetchNFTs = async (isNext) => {
+    if(collection.length === 0 && wallet.length === 0) {
+      return;
     }
 
-    if(collection.length) {
-      var requestOptions = {
+    try {
+      const method = collection.length > 0 && wallet.length === 0 ? "getNFTsForCollection" : "getNFTs";
+      const baseURL = `https://eth-mainnet.alchemyapi.io/v2/${process.env.NEXT_PUBLIC_API_KEY}/${method}/`;
+      let requestOptions = {
         method: 'GET',
         redirect: 'follow'
       };
 
-      const baseURL = `https://eth-mainnet.alchemyapi.io/v2/${process.env.NEXT_PUBLIC_API_KEY}/getNFTsForCollection/`;
-      const fetchURL = `${baseURL}?contractAddress=${collection}&withMetadata=${"true"}&startToken=${startToken}`;
-      const nfts = await fetch(fetchURL, requestOptions).then(data => data.json())
+      let fetchURL;
+
+      if(collection.length > 0 && wallet.length === 0) {
+        fetchURL = `${baseURL}?contractAddress=${collection}&withMetadata=true`;
+      } else if(!collection.length) {
+        fetchURL = `${baseURL}?owner=${wallet}`;
+      } else {
+        fetchURL = `${baseURL}?owner=${wallet}&contractAddresses%5B%5D=${collection}`;
+      }
+
+      if (isNext && startToken) {
+        if (method === "getNFTsForCollection") {
+          fetchURL = `${fetchURL}&startToken=${startToken}`;
+        } else if (method === "getNFTs") {
+          fetchURL = `${fetchURL}&pageKey=${startToken}`;
+        }
+      } else {
+        setStartToken(null);
+      }
+
+      const nfts = await fetch(fetchURL, requestOptions).then(data => data.json());
+
       if(nfts) {
         console.log(nfts.nextToken)
-
-        if(nfts.nextToken) {
-          setStartToken(nfts.nextToken)
-        }
-        console.log(NFTs.length)
-        console.log("NFTs in collection:", nfts)
-
-        if(NFTs.length > 0) {
+        if(nfts.nfts) {
+        if(isNext) {
           setNFTs([...NFTs,...nfts.nfts])
+          console.log(NFTs.length)
+          console.log("NFTs in collection:", nfts)  
         } else {
         setNFTs(nfts.nfts)
         }
-
+        setStartToken(nfts.nextToken)
+      } else if (nfts.ownedNfts) {
+        if(isNext) {
+          setNFTs([...NFTs, ...nfts.ownedNfts]);
+        } else {
+          setNFTs(nfts.ownedNfts);
+        }
+          setStartToken(nfts.pageKey)
+        }
       }
-    }
-  }
-
-  // Fetch API
-
- /* const fetchNFTsForCollection = async () => {
+    } catch (err) {
+      console.log(err);
+    } 
     
-  } */
+  }
 
   const onKeyPress = (e) => {
     if(e.which === 13) {
@@ -129,6 +127,7 @@ const Home = () => {
         
           Show NFTs</button>
       </div>
+
       <div className="flex flex-wrap gap-y-10 mt-4 w-5/6 gap-x-6 justify-center bg-white rounded-md">
         {
           NFTs.map((nft, index) => {
@@ -137,7 +136,7 @@ const Home = () => {
             )
           })
         }
-    
+        
       {startToken ? 
           <button 
             className={"text-white bg-blue-500 hover:bg-blue-600 active:bg-blue-700 px-4 py-2 mt-6 mb-7 rounded-md w-3/4"}
@@ -146,7 +145,7 @@ const Home = () => {
           </button>
           : <></> } 
       </div>
-    </div>
+      </div>
 
       {showButton && (
         <svg 
